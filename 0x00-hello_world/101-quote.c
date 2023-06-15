e7f330c36e32d293fff3581abe201d99d897a57f
aboubakr <c.aboubakr@gmail.com>
2023-06-15 13:41:31 +0100

    Adding file 101-quote.c that use write function to print message in STDERR

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t len = strlen(message);
+write(STDERR_FILENO, message, len);
+return (1);
+}
