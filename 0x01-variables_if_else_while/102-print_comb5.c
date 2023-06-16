fbc05e45498c599e2223619c65ab6951cd5a84bc
aboubakr <c.aboubakr@gmail.com>
2023-06-16 23:56:22 +0100

    Modification file 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Return1: 1 in Errors
+ */
 int main(void)
 {
-int i, j, k, l;
-for (i = 0; i < 10; i++)
+int i, j;
+for (i = 0; i < 100; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j < 100; j++)
 {
-for (k = 0; k < 10; k++)
+if (i < j)
 {
-for (l = 0; l < 10; l++)
-{
-if ((i != k || j != l ) && (k >= i && l >= j) )
-{
-putchar(i + '0');
-putchar(j + '0');
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
-if (i == 9 && j == 8 && k == 9 && l == 9)
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
 {
-continue;
-}
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
