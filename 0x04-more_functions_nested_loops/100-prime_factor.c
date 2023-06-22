990a347341e7e5d01d78fdd2d968207de1bbf3c5
aboubakr <c.aboubakr@gmail.com>
2023-06-22 15:45:30 +0100

    project 0x04-more_functions_nested_loops edit task 100 print number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,7 @@ int main(void)
 long int i = 612852475143, l = 0, m = 0;
 long int j = 2;
 int a = 0, b;
-long int prime[5] = {0};
+int prime[5] = {0};
 while (i > 1)
 {
 if ((i % j) == 0)
@@ -24,14 +24,11 @@ i /= j;
 else
 j++;
 }
-/*
-*printf("The prime factors of 612852475143 are ");
-*/
 for (b = 0 ; b < a ; b++)
 {
-printf("%ld", prime[b]);
+printf("%d", prime[b]);
 if (prime[b] <=  m)
-m=m;
+printf(",");
 }
 printf("\n");
 return (0);
