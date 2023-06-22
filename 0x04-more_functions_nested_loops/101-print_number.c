680ca6c94ea882a272b278acb9f34e65b3bdd0c7
aboubakr <c.aboubakr@gmail.com>
2023-06-22 14:39:37 +0100

    project 0x04-more_functions_nested_loops edit task 101 print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,35 +8,39 @@ void print_number(int n)
 int a;
 a = n;
 if (n < 0)
+{
 a = n * -1;
+_putchar('-');
+}
 if (a >= 0 && a < 10)
 {
-if (n < 0)
-_putchar('-');
 _putchar('0' + a);
 }
 else if (a >= 10 && a < 100)
 {
-if (n < 0)
-_putchar('-');
 _putchar('0' + a / 10);
 _putchar('0' +  a % 10);
 }
 else if (a >= 100 && a < 1000)
 {
-if (n < 0)
-_putchar('-');
 _putchar('0' + a / 100);
 _putchar('0' + (a % 100) / 10);
 _putchar('0' +  a % 10);
 }
 else if (a >= 1000 && a < 9999)
 {
-if (n < 0)
-_putchar('-');
 _putchar('0' + a / 1000);
 _putchar('0' + (a % 1000) / 100);
 _putchar('0' + (a % 100) / 10);
 _putchar('0' + a % 10);
 }
+else if (a >= 10000 && a < 99999)
+{
+_putchar('0' + a / 10000);
+_putchar('0' + (a / 1000) % 10);
+_putchar('0' + (a / 100)  % 10);
+_putchar('0' + (a / 10) % 10);
+_putchar('0' + a % 10);
 }
+}
+
