771a7454197f530b3225821dd3640ba50a9faa66
aboubakr <c.aboubakr@gmail.com>
2023-06-22 14:52:02 +0100

    project 0x04-more_functions_nested_loops edit task 101 print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,54 +1,26 @@
 #include "main.h"
+
 /**
-* print_number - prints an integer.
-* @n:integer to print
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-int a = n;
-if (n < 0)
-{
-a = n * -1;
-_putchar('-');
-}
-if (a >= 0 && a < 10)
-{
-_putchar('0' + a);
-}
-else if (a >= 10 && a < 100)
-{
-_putchar('0' + a / 10);
-_putchar('0' +  a % 10);
-}
-else if (a >= 100 && a < 1000)
-{
-_putchar('0' + a / 100);
-_putchar('0' + (a % 100) / 10);
-_putchar('0' +  a % 10);
-}
-else if (a >= 1000 && a < 9999)
-{
-_putchar('0' + a / 1000);
-_putchar('0' + (a % 1000) / 100);
-_putchar('0' + (a % 100) / 10);
-_putchar('0' + a % 10);
-}
-else if (a >= 10000 && a < 99999)
-{
-_putchar('0' + a / 10000);
-_putchar('0' + (a / 1000) % 10);
-_putchar('0' + (a / 100)  % 10);
-_putchar('0' + (a / 10) % 10);
-_putchar('0' + a % 10);
-}
-else if (a >= 100000 && a < 999999)
-{
-_putchar('0' + a / 100000);
-_putchar('0' + (a / 10000) % 10);
-_putchar('0' + (a / 1000) % 10);
-_putchar('0' + (a / 100)  % 10);
-_putchar('0' + (a / 10) % 10);
-_putchar('0' + a % 10);
-}
-}
+	int divisor = 1;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor >= 10)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}
