bd597e04b69219066ffd59887abddaeafe9cfa32
aboubakr <c.aboubakr@gmail.com>
2023-06-26 15:11:24 +0100

    project 0x05 pointer array and string : edit file 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print_rev - prints a string, followed by a new line, to stdout.
- * @s: A pointer to a string
+ * @str: A pointer to a string
  */
 void print_rev(char *str)
 {
-int l = 0, i=0;
- for (i = 0; str[i] != '\0'; i++)
+int l = 0, i = 0;
+for (i = 0; str[i] != '\0'; i++)
 {
 l++;
 }
 _putchar(l + 48);
-for (i=l-1;i>=0;i--)
+for (i = l - 1; i >= 0; i--)
 {
 _putchar(str[i]);
 }
