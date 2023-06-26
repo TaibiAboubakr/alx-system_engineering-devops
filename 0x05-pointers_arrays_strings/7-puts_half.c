379bf6a8a6fd6872bc65e02fdef2a4a1ec6d5ee7
aboubakr <c.aboubakr@gmail.com>
2023-06-26 16:09:56 +0100

    project 0x05 pointer array and string : create file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: A pointer to a string
+ */
+void puts_half(char *str)
+{
+char *st = str;
+int l = 0, h = 0, i;
+while (*st != '\0')
+{
+l++;
+st++;
+}
+if (l % 2 == 0)
+{
+h = l / 2;
+for (i = h; i < l; i++)
+_putchar(str[i]);
+}
+else
+{
+h = (l - 1) / 2;
+for (i = h; i < l; i++)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
