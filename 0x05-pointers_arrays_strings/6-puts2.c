74ca9a7105fa77f7be836b2910f85ccf78d3807d
aboubakr <c.aboubakr@gmail.com>
2023-06-26 15:50:48 +0100

    project 0x05 pointer array and string : create file 6-puts.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: A pointer to a string
+ */
+void puts2(char *str)
+{
+char *st = str;
+while (*st != '\0')
+{
+_putchar(*st);
+st += 2;
+}
+_putchar('\n');
+}
