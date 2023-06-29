88876d5a6a7e2fb578591a9ae6357f2482eeca2e
aboubakr <c.aboubakr@gmail.com>
2023-06-29 23:17:22 +0100

    104 project 0x06

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+ * print_buffer - function that prints a buffer.
+ * @b: A pointer to a string
+ * @size: size bytes of the buffer pointed by b
+ */
+void print_buffer(char *b, int size)
+{
+int i = 0;
+while (*b != '\0')
+i++;
+if (i > size)
+size = 0;
+}
