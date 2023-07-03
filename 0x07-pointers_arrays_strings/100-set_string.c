fc1b079b141ec58c6168cb7cfb3ffee4b9b09dd2
aboubakr <c.aboubakr@gmail.com>
2023-07-03 15:11:27 +0100

    Project 0x07-pointers arrays strings : Edit file 100-set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,22 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_diagsums - function that sets the value of a pointer to a char.
- * the sum of the two diagonals of a square matrix of integers.
- * @a: pointer to a multidimensional array
- * @size: size of array
+ * set_string - function that sets the value of a pointer to a char..
+ * @to: pointer to an string
+ * @s: pointer to a pointer
  */
 void set_string(char **s, char *to)
 {
-int i, sum1 = 0, idx = 0, idx2 = 0, sum2 = 0;
-for (i = 0; i < size ; i++)
+int i = 0, j = 0, k = 0, l;
+while ((*s)[j] != '\0')
+j++;
+while (to[i] != '\0')
+i++;
+l = i;
+for (k = 0; k < l ; k++)
 {
-idx = i * size + i;
-sum1 += a[idx];
-idx2 = (i + 1) * size - (i + 1);
-sum2 += a[idx2];
+*s = to;
 }
-printf("%d, ", sum1);
-printf("%d", sum2);
-printf("\n");
 }
