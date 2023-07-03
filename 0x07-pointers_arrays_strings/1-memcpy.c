fe4486f7473f4ebf9cb638f67faa1e9509e208e9
aboubakr <c.aboubakr@gmail.com>
2023-07-03 12:47:27 +0100

    Project 0x07-pointers arrays strings : Edit file 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,21 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
+unsigned int i = 0, j = 0;
 char *pdest = dest;
-
+while (src[j])
+j++;
 for (; i < n && *src != '\0'; i++)
 {
 *dest = *src;
 dest++;
 src++;
+}
+while (n > j)
+{
+*dest = '\0';
+dest++;
+j++;
 }
 return (pdest);
 }
