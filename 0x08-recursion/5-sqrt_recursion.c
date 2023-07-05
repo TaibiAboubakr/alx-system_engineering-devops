d5a43120867c0e49a0548184d81f05e5087b819f
aboubakr <c.aboubakr@gmail.com>
2023-07-05 19:55:14 +0100

    Task 5 project 0x08 recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @x: integer number
+ * Return: natural square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 1)
+return (1);
+if (n == -1)
+return (-1);
+
+return (_sqrt(n, n - 1));
+}
+/**
+ * _sqrt- recursive for _sqrt_recursion
+ * @n: integer number to check
+ * @i: integer number to divise
+ * Return: 1 if prime, 0 otherwise
+*/
+int _sqrt(int n, int i)
+{
+if (i == 1 )
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt(n, i - 1));
+
+}
+
