703c4d04ad48469428cd9ef43c53520298250f39
aboubakr <c.aboubakr@gmail.com>
2024-01-01 12:42:58 +0100

    update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install flask 
+#!/usr/bin/puppet
+# Install Flask and Werkzeug
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
