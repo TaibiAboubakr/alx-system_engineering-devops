7a3c09523aeeafa5102b56bc726e12a9e4826963
aboubakr <c.aboubakr@gmail.com>
2024-01-09 01:01:26 +0100

    update the file 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -46,7 +46,12 @@ file { '/etc/nginx/sites-available/default':
 }
 EOT
 }
-
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart nginx service'],
+}
 file { '/var/www/html/404.html':
   ensure  => present,
   path    => '/var/www/html/404.html',
