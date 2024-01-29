cdf4afccde274facaef0095b8a6b6f04a6cff042
aboubakr <c.aboubakr@gmail.com>
2024-01-29 22:27:27 +0100

    update the file 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+returns information about his/her 'TODO' list progress."""
 
-import sys
 import requests
+import sys
 """  Importing modules """
 
 
