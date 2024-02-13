8b7c33f102e9c606afd84b3baecf8f1ad98ac331
aboubakr <c.aboubakr@gmail.com>
2024-02-13 12:43:33 +0100

    add the file 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  function that queries the Reddit API and returns the number of subscribers """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API and 
+    returns the number of subscribers """
+    apiurl = f"https://www.reddit.com/r/{subreddit}/about.json"
+    try:
+        response = requests.get(apiurl)
+        content = response.json()
+        
+        if 'data' in content and 'subscribers' in content['data']:
+            return content['data']['subscribers']
+        else:
+            return 
+    except Exception as e:
+        return 0
+    
