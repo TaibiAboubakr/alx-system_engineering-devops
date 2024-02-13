80dbc72d473aa68bb64c7d9387b73b52466dabd5
aboubakr <c.aboubakr@gmail.com>
2024-02-13 14:08:49 +0100

    update the file 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,10 @@ def number_of_subscribers(subreddit):
     returns the number of subscribers """
     if type(subreddit) is not str or subreddit is None:
         return 0
-    apiurl = f"http://www.reddit.com/r/{subreddit}/about.json"
-    re = requests.get(apiurl,
-                      headers={'User-Agent': '0x16-api_advanced:project:v1.0.0\
-                           (by /u/taibi_aboubakr)'}).json()
+    apiurl = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {
+        'User-Agent': '0x16-api_advanced:project:v1.0.0(by /u/taibi_aboubakr)'
+        }
+    re = requests.get(apiurl, headers=header, allow_redirects=False).json()
     num_subscribers = re.get("data", {}).get("subscribers", 0)
     return num_subscribers
